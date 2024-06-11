a52662457f8d41e7a4d61829e847956efb91892b
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-06-11 17:30:12 +1000

    updates

--- scripts/telostas.sh
+++ scripts/telostas.sh
@@ -9,11 +9,19 @@ die(){
 
 FILE=$1
 
-echo "total telomere regions at the end of contigs"
-cat $FILE | wc -l
+test -f $FILE || die "File $FILE not found"
+PREFIX=$(basename $FILE .fasta)
+BED=${PREFIX}/${PREFIX}.windows.0.4.50kb.ends.bed
+
+/install/vgp-pipeline/telomere/telomere_analysis.sh ${PREFIX} 0.4 50000 ${FILE}
+test -e ${BED} || die "telomere_analysis.sh failed"
+
+echo -e "FILE\t${FILE}"
+echo -e -n "total telomere regions at the end of contigs:\t"
+cat $BED | wc -l
 echo ""
 echo ""
-cat $FILE | cut -f 1  | sort | uniq -c | awk 'BEGIN{t1=0;t2=0;t3=0}{if($1==1){t1+=1}else if($1==2){t2+=1} else {t3+=1}} END{print "telo in one end:\t"t1"\ntelo in two ends:\t"t2"\ntelo more than 2 (must be 0):\t"t3"\n"}'
+cat $BED | cut -f 1  | sort | uniq -c | awk 'BEGIN{t1=0;t2=0;t3=0}{if($1==1){t1+=1}else if($1==2){t2+=1} else {t3+=1}} END{print "telo in one end:\t"t1"\ntelo in two ends:\t"t2"\ntelo more than 2 (must be 0):\t"t3"\n"}'
 
 
 
