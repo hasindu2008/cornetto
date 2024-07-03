080d9663ac3ae8d4d63b677d93fed049a42b74b2
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-07-03 16:17:24 +1000

    polish gadi

--- scripts/getstat.pbs.sh
+++ scripts/getstat.pbs.sh
@@ -34,6 +34,8 @@ usage() {
 
 module load minimap2/2.24
 module load samtools/1.12
+module load bedtools/2.28.0
+module load java/jdk-17.0.2
 
 ###################################################################
 
@@ -50,10 +52,17 @@ samtools --version || die "Could not find samtools"
 export MINIDOT=/g/data/ox63/install/miniasm/minidot
 export PATH=$PATH:/g/data/ox63/install/datamash
 export TELO_SCRIPT_PATH=/g/data/ox63/install/vgp-pipeline/telomere/telomere_analysis.sh
-export SCRIPT_DIR=/g/data/ox63/hasindu2008.git/cornetto/scripts
+export SCRIPT_DIR=/g/data/ox63/hasindu/cornetto/cornetto/scripts
 
-${SCRIPT_DIR}/minidotplot.sh ${REF} ${ASM}  || die "Failed to run minidotplot"
+test -z $REF && die "Reference file not provided"
+test -z $ASM && die "Assembly file not provided"
 
-${SCRIPT_DIR}/telostas.sh ${ASM}  || die "Failed to run telostas"
+FILENAME=$(basename ${ASM})
+test -e ${ASM} || cp ${ASM} ${FILENAME}
+test -e ${FILENAME} ||  die "Assembly file not available in the current directory"
 
-${SCRIPT_DIR}/asmstats.sh ${ASM} > ${ASM}.asmstats.txt || die "Failed to run asmstats"
+${SCRIPT_DIR}/minidotplot.sh ${REF} ${FILENAME}  || die "Failed to run minidotplot"
+
+${SCRIPT_DIR}/telostas.sh ${FILENAME}  || die "Failed to run telostas"
+
+${SCRIPT_DIR}/asmstats.sh ${FILENAME} > ${FILENAME}.asmstats.txt || die "Failed to run asmstats"
