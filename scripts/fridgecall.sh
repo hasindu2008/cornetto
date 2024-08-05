edf294daaba32be1c35607dbb9c48e2fc8925bda
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-08-05 22:29:06 +1000

    Create fridgecall.sh

---
+++ scripts/fridgecall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# run inside a screen session
+
+PREFIX=A_1
+SAMPLE=QGXHXX240275
+
+cd /data3/cornetto
+slow5tools merge /data/QGXHXX240275/*/*/slow5/ -o ${PREFIX}_${SAMPLE}.blow5
+
+
+
