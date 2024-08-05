f0d9bd1b2e9256710a497ba5b9516b2a74c3b90e
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-08-06 09:55:52 +1000

    autoscript

--- scripts/autocall/autocall.sh
+++ scripts/autocall/autocall.sh
@@ -8,8 +8,11 @@ die() {
 [ $# -ne 1 ] && die "Usage: $0 <prefix_sample>"
 
 NAME=$1
-DATA_PATH=/directflow/KCCGGenometechTemp/projects/iradev/operation_cornetto/temp_hasindu/
+DATA_PATH=/directflow/KCCGGenometechTemp/projects/iradev/operation_cornetto/autocall_hasindu/
+FRIDGE_PATH=/data3/cornetto/
 SCRIPT_PATH=$(dirname "$0")
+
+
 test -e $SCRIPT_PATH/fridge_dload.sge.sh || die "Could not find $SCRIPT_PATH/fridge_dload.sge.sh"
 test -e $SCRIPT_PATH/split_dorado_duplex.sge.sh || die "Could not find $SCRIPT_PATH/split_dorado_duplex.sge.sh"
 
@@ -17,7 +20,7 @@ cd $DATA_PATH/ || die "Could not cd to $DATA_PATH"
 mkdir ${NAME} || die "Could not create ${NAME}"
 cd ${NAME} || die "Could not cd to ${NAME}"
 
-qsub -sync y $SCRIPT_PATH/fridge_dload.sge.sh /data3/cornetto/${NAME}.blow5 ./ || die "Could not download the data"
+qsub -sync y $SCRIPT_PATH/fridge_dload.sge.sh ${FRIDGE_PATH}/${NAME}.blow5 ./ || die "Could not download the data"
 qsub -sync y $SCRIPT_PATH/split_dorado_duplex.sge.sh ${NAME} || die "Could not basecall the data"
 
 echo "Done"
