d681568876579479858ce79a3619c44c92e6fe5a
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-08-07 16:45:21 +1000

    update

--- scripts/autocall/launch.sh
+++ scripts/autocall/launch.sh
@@ -38,7 +38,7 @@ cd ${FRIDGE_TMP} || die "Could not cd to ${FRIDGE_TMP}"
 slow5tools merge /data/${SAMPLE}/*/*/slow5/ -o ${PREFIX}_${SAMPLE}.blow5 || die "Could not merge slow5 files"
 slow5tools stats ${PREFIX}_${SAMPLE}.blow5 || die "Could not get stats"
 
-COMMAND="screen -S autocall_${PREFIX}_${SAMPLE} -d -m -L ${BRENNER_SCRIPT} ${NAME}"
+COMMAND="source /etc/profile; screen -S autocall_${PREFIX}_${SAMPLE} -d -m -L ${BRENNER_SCRIPT} ${NAME}"
 echo "$COMMAND"
 ssh brenner-fpga "$COMMAND"
 
