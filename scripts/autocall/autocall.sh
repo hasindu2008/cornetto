a9441901b8f8858dfadc4da63ab1c7d129ba6d18
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-08-07 15:40:20 +1000

    update

--- scripts/autocall/autocall.sh
+++ scripts/autocall/autocall.sh
@@ -8,9 +8,9 @@ die() {
 [ $# -ne 1 ] && die "Usage: $0 <prefix_sample>"
 
 NAME=$1
-DATA_PATH=/directflow/KCCGGenometechTemp/projects/iradev/operation_cornetto/autocall_hasindu/
-FRIDGE_PATH=/data3/cornetto/
-GADI_PATH=/g/data/ox63/hasindu/cornetto/autocall
+BRENNER_DATA=/directflow/KCCGGenometechTemp/projects/iradev/operation_cornetto/autocall_hasindu/
+FRIDGE_TMP=/data3/cornetto/
+GADI_DATA=/g/data/ox63/hasindu/cornetto/autocall
 
 SCRIPT_REALPATH=$(realpath "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT_REALPATH")
@@ -19,11 +19,11 @@ test -e $SCRIPT_PATH/fridge_dload.sge.sh || die "Could not find $SCRIPT_PATH/fri
 test -e $SCRIPT_PATH/split_dorado_duplex.sge.sh || die "Could not find $SCRIPT_PATH/split_dorado_duplex.sge.sh"
 test -e $SCRIPT_PATH/get_duplex_and_simplex_reads.sge.sh || die "Could not find $SCRIPT_PATH/get_duplex_and_simplex_reads.sge.sh"
 
-cd $DATA_PATH/ || die "Could not cd to $DATA_PATH"
+cd $BRENNER_DATA/ || die "Could not cd to $BRENNER_DATA"
 mkdir ${NAME} || die "Could not create ${NAME}"
 cd ${NAME} || die "Could not cd to ${NAME}"
 
-qsub -sync y $SCRIPT_PATH/fridge_dload.sge.sh ${FRIDGE_PATH}/${NAME}.blow5 ./ || die "Could not download the data"
+qsub -sync y $SCRIPT_PATH/fridge_dload.sge.sh ${FRIDGE_TMP}/${NAME}.blow5 ./ || die "Could not download the data"
 qsub -sync y $SCRIPT_PATH/split_dorado_duplex.sge.sh ${NAME} || die "Could not basecall the data"
 
 #handle those failed basecall files if any?
@@ -31,8 +31,7 @@ qsub -sync y $SCRIPT_PATH/split_dorado_duplex.sge.sh ${NAME} || die "Could not b
 
 qsub -sync y $SCRIPT_PATH/get_duplex_and_simplex_reads.sge.sh ${NAME} || die "Could not basecall the data"
 
-ssh gadi "mkdir ${GADI_PATH}/${NAME}" || die "gadi ssh failed"
-scp ${NAME}.duplex_reads.fastq gadi-dm:${GADI_PATH}/${NAME}/ || die "copying to Gadi failed"
-
+ssh gadi "mkdir ${GADI_DATA}/${NAME}" || die "gadi ssh failed"
+scp ${NAME}.duplex_reads.fastq gadi-dm:${GADI_DATA}/${NAME}/ || die "copying to Gadi failed"
 
 echo "Done"
---
+++ scripts/autocall/launch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+die() {
+    echo "$1"
+    exit 1
+}
+
+## On fridge: run inside a screen session
+
+#PREFIX=A_1
+#SAMPLE=QGXHXX240275
+
+test $# -eq 2 || die "Usage: $0 <prefix:A_1> <sample:QGXHXX240275>"
+
+PREFIX=$1
+SAMPLE=$2
+
+FRIDGE_TMP=/data3/cornetto
+BRENNER_SCRIPT=/home/hasgam/hasindu2008.git/cornetto/scripts/autocall/autocall.sh
+BRENNER_DATA=/directflow/KCCGGenometechTemp/projects/iradev/operation_cornetto/autocall_hasindu/
+GADI_DATA=/g/data/ox63/hasindu/cornetto/autocall
+
+NAME=${PREFIX}_${SAMPLE}
+
+checkshit() {
+    test -d ${FRIDGE_TMP} || die "${FRIDGE_TMP} not found on fridge"
+    ssh brenner-fpga "test -x ${BRENNER_SCRIPT}" || die "${BRENNER_SCRIPT} not found on brenner-fpga"
+    ssh brenner-fpga "test -d ${BRENNER_DATA}" || die "${BRENNER_DATA} not found on brenner-fpga"
+    ssh brenner-fpga "test -d ${BRENNER_DATA}/${NAME}" && die "${BRENNER_DATA}/${NAME} already exists on brenner-fpga. Delete that shit first"
+    ssh gadi "test -d ${GADI_DATA}" || die "${GADI_DATA} not found on gadi"
+    ssh gadi "test -d ${GADI_DATA}/${NAME}" && die "${GADI_DATA}/${NAME} already exists on gadi. Delete that shit first"
+
+}
+
+cd ${FRIDGE_TMP} || die "Could not cd to ${FRIDGE_TMP}"
+slow5tools merge /data/${SAMPLE}/*/*/slow5/ -o ${PREFIX}_${SAMPLE}.blow5 || die "Could not merge slow5 files"
+slow5tools stats ${PREFIX}_${SAMPLE}.blow5 || die "Could not get stats"
+
+COMMAND="screen -S autocall_${PREFIX}_${SAMPLE} -d -m -L ${BRENNER_SCRIPT} ${NAME}"
+echo "$COMMAND"
+ssh brenner-fpga "$COMMAND"
+
+echo "Handed the work to the brenner-fpga"
