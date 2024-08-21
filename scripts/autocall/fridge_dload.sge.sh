0b525ca00f09216bd95b6ea1633fa6272d8c2434
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-08-21 14:09:04 +1000

    fridge

--- scripts/autocall/fridge_dload.sge.sh
+++ scripts/autocall/fridge_dload.sge.sh
@@ -10,29 +10,29 @@
 #$ -pe smp 1
 
 #############################################################################################
-#                                                                                         
-# copy data from fridge to cluster storage                                                
-#                                                                                         
-# setting up: first, you have to setup the ssh key-based access from cluster to gridION      
-#		1. generate a ssh key if you haven't done so									  
-#				ssh-keygen                                                                
+#
+# copy data from fridge to cluster storage
+#
+# setting up: first, you have to setup the ssh key-based access from cluster to gridION
+#		1. generate a ssh key if you haven't done so
+#				ssh-keygen
 #		2. start a session on a node with 10Gbps access on clus2020ter that has access to gridION
-#				qrsh -l fast_dm=10                                                        
-#		3. copy the ssh key to grdION 														  
-#				ssh-copy-id gtgadmin@129.94.120.250 -p 2020                                 
-#		4. check if password-less access work											  
-#				ssh  gtgadmin@129.94.120.250 -p 2020                                            
-#		5. logout from gridION and then from the qrsh session                                                 
-#				ctrl+d twice                                                              
-#                                                                                         
-#                                                                                         
-# usage: qsub ./grid_dload.sge.sh /path/on/gridion /path/on/cluster/storage               
-#                                                                                         
+#				qrsh -l fast_dm=10
+#		3. copy the ssh key to grdION
+#				ssh-copy-id gtgadmin@129.94.120.250 -p 2020
+#		4. check if password-less access work
+#				ssh  gtgadmin@129.94.120.250 -p 2020
+#		5. logout from gridION and then from the qrsh session
+#				ctrl+d twice
+#
+#
+# usage: qsub ./grid_dload.sge.sh /path/on/gridion /path/on/cluster/storage
+#
 #############################################################################################
 
 SOURCE=$1
 DEST=$2
 
-rsync -a --progress --rsh='ssh -T -o Compression=no -x -p 2020' hasindu@129.94.120.250:${SOURCE} ${DEST}
+rsync -a --progress --rsh='ssh -T -o Compression=no -x -p 2020' fridge:${SOURCE} ${DEST}
 
 
