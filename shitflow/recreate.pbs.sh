8d046f2de9b8a27ae5b4740e0aabd34db32a8125
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2025-01-06 17:25:48 +1100

    fix

--- shitflow/recreate.pbs.sh
+++ shitflow/recreate.pbs.sh
@@ -9,23 +9,27 @@
 #PBS -l storage=gdata/if89+scratch/wv19+gdata/wv19+gdata/ox63
 
 usage() {
-	echo "Usage: qsub -v FISH_NOW=A_3_QGXHXX240283,PREFIX=hg002-cornetto-A_3 ./generate_panel.pbs.sh" >&2
+	echo "Usage: qsub -v ASM=hg002-cornetto-A_3 ./recreate.pbs.sh" >&2
 	echo
 	exit 1
 }
 
-[ -z "${PREFIX}" ] && usage
-[ -z "${FISH_NOW}" ] && usage
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ -z "${ASM}" ] && usage
 
 module load bedtools/2.28.0
 module load minimap2/2.24
 module load samtools/1.19
 
-ONT_DATADIR=/g/data/ox63/hasindu/cornetto/shitflow/
+#ONT_DATADIR=/g/data/ox63/hasindu/cornetto/shitflow/
 export SCRIPT_DIR=/g/data/ox63/hasindu/cornetto/cornetto/scripts
 
-cd ${ONT_DATADIR}/${FISH_NOW}
-
+#cd ${ONT_DATADIR}/${FISH_NOW}
+test -f ${ASM}.fasta || die "Assembly ${ASM}.fasta not found"
 minimap2 -x map-ont -d ${ASM}.fasta.idx ${ASM}.fasta || die "minimap2 failed"
-${SCRIPT_DIR}/recreate-cornetto.sh ${ASM} || die "create-cornetto.sh failed"
+${SCRIPT_DIR}/recreate-cornetto.sh ${ASM}.fasta || die "create-cornetto.sh failed"
 
