f1ddcefad40fe89bec0ec412f26a5ce981446ebc
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2025-03-05 20:02:51 +1100

    recreate

---
+++ shitflow/saliva/recreate-combined-panel.pbs.sh
@@ -0,0 +1,62 @@
+
+#PBS -P ox63
+#PBS -q normal
+#PBS -N index
+#PBS -l walltime=4:00:00
+#PBS -l storage=gdata/ox63+scratch/ox63+scratch/if89+gdata/if89+gdata/te53
+#PBS -l mem=36GB
+#PBS -l ncpus=8
+#PBS -l wd
+
+THREADS=${PBS_NCPUS}
+module load minimap2/2.24
+
+set -o pipefail
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+export PATH=$PATH:/g/data/te53/ontsv/sv_parsing/scripts/
+
+usage() {
+	echo "Usage: qsub -v HUMAN_ASM=saliva-Q_1,NONHUMAN_PREFIX=/path/to/all-crap/saliva-Q_0, ./script.pbs.sh" >&2
+	echo
+	exit 1
+}
+
+die () {
+	echo >&2 "$@"
+	exit 1
+}
+
+ASM=${HUMAN_ASM}
+HUMAN=../
+
+mkdir -p human_plus_nonhuman || die "Failed to create human_plus_nonhuman"
+cd human_plus_nonhuman || die "Failed to cd human_plus_nonhuman"
+
+HUMAN_ASM=${HUMAN}/${ASM}.fasta
+HUMAN_BORING_BED=${HUMAN}/${ASM}_dip.boringbits.bed
+
+NONHUMAN_CTG_FA=${NONHUMAN_PREFIX}.nonhuman_contigs.fasta
+NONHUMAN_CTG_BED=${NONHUMAN_PREFIX}.nonhuman_contigs.bed
+
+COMBINED_FA=${ASM}.plus_nonhuman_ctg.fasta
+COMBINED_BORING_BED=${ASM}_dip.boringbits.plus_nonhuman_ctg.bed
+COMBINED_BORING_TXT=${ASM}_dip.boringbits.plus_nonhuman_ctg.txt
+
+flattenFasta.pl -tab ${NONHUMAN_CTG_FA} | awk '{print $1"_nonhuman\t"$2}' > tmp1.tab || die "Failed to flatten ${NONHUMAN_CTG_FA}"
+flattenFasta.pl -fa tmp1.tab > ${ASM}_nonhuman_ctg_renamed.fasta || die "Failed to create ${ASM}_nonhuman_ctg_renamed"
+
+cat ${NONHUMAN_CTG_BED} | awk '{print $1"_nonhuman\t"$2"\t"$3}' > ${ASM}_nonhuman_ctg_renamed.bed || die "Failed to create ${ASM}_nonhuman_ctg_renamed.bed"
+
+cat ${HUMAN_ASM} ${ASM}_nonhuman_ctg_renamed.fasta > ${COMBINED_FA} || die "Failed to create ${COMBINED_FA}"
+cat ${HUMAN_BORING_BED} ${ASM}_nonhuman_ctg_renamed.bed > ${COMBINED_BORING_BED} || die "Failed to create ${COMBINED_BORING_BED}"
+
+cat ${COMBINED_BORING_BED} | awk '{print $1","$2","$3",+"}' > plus_tmp1 || die "Failed to create plus_tmp1"
+cat ${COMBINED_BORING_BED} | awk '{print $1","$2","$3",-"}' > minus_tmp1 || die "Failed to create minus_tmp1"
+cat plus_tmp1 minus_tmp1 | sort > ${COMBINED_BORING_TXT} || die "Failed to create ${COMBINED_BORING_TXT}"
+
+minimap2 -x map-ont -d ${COMBINED_FA}.idx ${COMBINED_FA} || die "Failed to index ${COMBINED_FA}"
