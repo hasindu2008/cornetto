5f51915b443e82d81915fdf9c881fddecbc0b7d1
KavinduJayas <e17134@eng.pdn.ac.lk>
2025-03-27 15:55:36 +1100

    replace bash commands with 'fixdir'

--- scripts/minidotplot.sh
+++ scripts/minidotplot.sh
@@ -11,6 +11,7 @@ die() {
 
 MINIMAP2=minimap2
 SAMTOOLS=samtools
+CORNETTO=cornetto
 test -z ${MINIDOT} && MINIDOT=/install/miniasm/minidot
 
 REF=$1
@@ -28,18 +29,9 @@ test -e $MINIDOT  > /dev/null 2>&1 || die "minidot not found"
 ${MINIMAP2} -t16 --eqx -cx asm5 $REF $ASM > ${PREFIX}.tmp.paf || die "minimap2 failed"
 cut -f 1 ${PREFIX}.tmp.paf | sort -u > ${PREFIX}.tmp.ctg.list
 
-while read p;
-do
-    grep -w $p ${PREFIX}.tmp.paf | awk 'BEGIN{sump=0;sumn=0} {if($5=="-"){sumn+=($9-$8)}else{sump+=($9-$8)}} END{if(sump>sumn){print $1"\t+"}else{print $1"\t-"}}'
-done < ${PREFIX}.tmp.ctg.list > ${PREFIX}.tmp.dir.txt
+${CORNETTO} fixdir ${ASM} ${PREFIX}.tmp.fix.fasta || die "cornetto failed" # TODO:direct fixdir output to stdout instead of corrected_contigs.fasta
 
-grep -w "+" ${PREFIX}.tmp.dir.txt | cut -f 1 > ${PREFIX}.tmp.ctg_plus.txt || die "grep failed"
-grep -w "-" ${PREFIX}.tmp.dir.txt | cut -f 1 > ${PREFIX}.tmp.ctg_mins.txt || die "grep failed"
-
-$SAMTOOLS faidx  ${PREFIX} -r ${PREFIX}.tmp.ctg_plus.txt > ${PREFIX}.tmp.fix.fasta || die "samtools failed"
-$SAMTOOLS faidx  ${PREFIX} -r ${PREFIX}.tmp.ctg_mins.txt  -i >> ${PREFIX}.tmp.fix.fasta || die "samtools failed"
-
-cat ${PREFIX}.tmp.ctg_plus.txt ${PREFIX}.tmp.ctg_mins.txt  | while read p;
+grep '^>' corrected_contigs.fasta | sed 's/^>//' | while read p;
 do
 echo -n -e "${p}\t"
 grep -w $p ${PREFIX}.tmp.paf | cut -f 6 | sort | uniq -c | sort -k1,1 -rn | head -1 | awk '{print $2}' | awk -F'_' '{print $1}'
@@ -47,7 +39,7 @@ done  > ${PREFIX}.chr.annotate.txt || die "grep failed"
 
 awk '{count[$2]++; if (count[$2] > 1) {print $1"\t"$2 "_" count[$2]-1} else {print $1"\t"$2 "_0"}}' ${PREFIX}.chr.annotate.txt > ${PREFIX}.chr.rename.txt || die "awk failed"
 
-awk '{print "s/"$1"/"$2"/g"}' ${PREFIX}.chr.rename.txt | sed -f - ${PREFIX}.tmp.fix.fasta > ${PREFIX}.tmp.renamed.fasta || die "sed failed"
+awk '{print "s/"$1"/"$2"/g"}' ${PREFIX}.chr.rename.txt | sed -f - corrected_contigs.fasta > ${PREFIX}.tmp.renamed.fasta || die "sed failed"
 
 $MINIMAP2 -t16 --eqx -cx asm5 $REF ${PREFIX}.tmp.renamed.fasta > ${PREFIX}.fix.tmp.paf || die "minimap2 failed"
 
