43ad7e98fe8bb42da80d661a63e04f768d9f085e
KavinduJayas <e17134@eng.pdn.ac.lk>
2025-03-28 13:46:32 +1100

    Update script to use fixdir with stdout

--- scripts/minidotplot.sh
+++ scripts/minidotplot.sh
@@ -27,9 +27,9 @@ $SAMTOOLS --version > /dev/null 2>&1 || die "samtools not found"
 ${MINIMAP2} -t16 --eqx -cx asm5 $REF $ASM > ${PREFIX}.tmp.paf || die "minimap2 failed"
 cut -f 1 ${PREFIX}.tmp.paf | sort -u > ${PREFIX}.tmp.ctg.list
 
-${CORNETTO} fixdir ${ASM} ${PREFIX}.tmp.paf || die "cornetto failed" # TODO:direct fixdir output to stdout instead of corrected_contigs.fasta
+${CORNETTO} fixdir ${ASM} ${PREFIX}.tmp.paf > ${PREFIX}.tmp.fix.fasta 2> ${PREFIX}.missing_sequences.log || die "cornetto failed"
 
-grep '^>' corrected_contigs.fasta | sed 's/^>//' | while read p;
+grep '^>' ${PREFIX}.tmp.fix.fasta | sed 's/^>//' | while read p;
 do
 echo -n -e "${p}\t"
 grep -w $p ${PREFIX}.tmp.paf | cut -f 6 | sort | uniq -c | sort -k1,1 -rn | head -1 | awk '{print $2}' | awk -F'_' '{print $1}'
@@ -37,7 +37,7 @@ done  > ${PREFIX}.chr.annotate.txt || die "grep failed"
 
 awk '{count[$2]++; if (count[$2] > 1) {print $1"\t"$2 "_" count[$2]-1} else {print $1"\t"$2 "_0"}}' ${PREFIX}.chr.annotate.txt > ${PREFIX}.chr.rename.txt || die "awk failed"
 
-awk '{print "s/"$1"/"$2"/g"}' ${PREFIX}.chr.rename.txt | sed -f - corrected_contigs.fasta > ${PREFIX}.tmp.renamed.fasta || die "sed failed"
+awk '{print "s/"$1"/"$2"/g"}' ${PREFIX}.chr.rename.txt | sed -f - ${PREFIX}.tmp.fix.fasta > ${PREFIX}.tmp.renamed.fasta || die "sed failed"
 
 $MINIMAP2 -t16 --eqx -cx asm5 $REF ${PREFIX}.tmp.renamed.fasta > ${PREFIX}.fix.tmp.paf || die "minimap2 failed"
 
