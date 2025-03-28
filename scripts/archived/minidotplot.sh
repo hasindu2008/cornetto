22a4541090a014fa36fbceba30f9fee5dc6b4d9a
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2025-03-28 16:16:53 +1100

    archived scripts

---
+++ scripts/archived/minidotplot.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -o pipefail
+
+die() {
+    echo "$1"
+    exit 1
+}
+
+[ $# -ne 2 ] && die "Usage: $0 <reference> <myassembly>"
+
+MINIMAP2=minimap2
+SAMTOOLS=samtools
+test -z ${MINIDOT} && MINIDOT=/install/miniasm/minidot
+
+REF=$1
+ASM=$2
+
+PREFIX=$(basename ${ASM})
+
+[ ! -f $REF ] && die "Reference $REF not found"
+[ ! -f $ASM ] && die "Assembly $ASM not found"
+
+$MINIMAP2 --version > /dev/null 2>&1 || die "minimap2 not found"
+$SAMTOOLS --version > /dev/null 2>&1 || die "samtools not found"
+test -e $MINIDOT  > /dev/null 2>&1 || die "minidot not found"
+
+${MINIMAP2} -t16 --eqx -cx asm5 $REF $ASM > ${PREFIX}.tmp.paf || die "minimap2 failed"
+cut -f 1 ${PREFIX}.tmp.paf | sort -u > ${PREFIX}.tmp.ctg.list
+
+while read p;
+do
+    grep -w $p ${PREFIX}.tmp.paf | awk 'BEGIN{sump=0;sumn=0} {if($5=="-"){sumn+=($9-$8)}else{sump+=($9-$8)}} END{if(sump>sumn){print $1"\t+"}else{print $1"\t-"}}'
+done < ${PREFIX}.tmp.ctg.list > ${PREFIX}.tmp.dir.txt
+
+grep -w "+" ${PREFIX}.tmp.dir.txt | cut -f 1 > ${PREFIX}.tmp.ctg_plus.txt || die "grep failed"
+grep -w "-" ${PREFIX}.tmp.dir.txt | cut -f 1 > ${PREFIX}.tmp.ctg_mins.txt || die "grep failed"
+
+$SAMTOOLS faidx  ${PREFIX} -r ${PREFIX}.tmp.ctg_plus.txt > ${PREFIX}.tmp.fix.fasta || die "samtools failed"
+$SAMTOOLS faidx  ${PREFIX} -r ${PREFIX}.tmp.ctg_mins.txt  -i >> ${PREFIX}.tmp.fix.fasta || die "samtools failed"
+
+cat ${PREFIX}.tmp.ctg_plus.txt ${PREFIX}.tmp.ctg_mins.txt  | while read p;
+do
+echo -n -e "${p}\t"
+grep -w $p ${PREFIX}.tmp.paf | cut -f 6 | sort | uniq -c | sort -k1,1 -rn | head -1 | awk '{print $2}' | awk -F'_' '{print $1}'
+done  > ${PREFIX}.chr.annotate.txt || die "grep failed"
+
+awk '{count[$2]++; if (count[$2] > 1) {print $1"\t"$2 "_" count[$2]-1} else {print $1"\t"$2 "_0"}}' ${PREFIX}.chr.annotate.txt > ${PREFIX}.chr.rename.txt || die "awk failed"
+
+awk '{print "s/"$1"/"$2"/g"}' ${PREFIX}.chr.rename.txt | sed -f - ${PREFIX}.tmp.fix.fasta > ${PREFIX}.tmp.renamed.fasta || die "sed failed"
+
+$MINIMAP2 -t16 --eqx -cx asm5 $REF ${PREFIX}.tmp.renamed.fasta > ${PREFIX}.fix.tmp.paf || die "minimap2 failed"
+
+${MINIDOT} ${PREFIX}.fix.tmp.paf -f 2  > ${PREFIX}.eps || die "minidot failed"
+
+echo "yey, all done"
---
+++ scripts/archived/telostats.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o pipefail
+
+die(){
+    echo $1
+    exit 1
+}
+
+[ $# -ne 1 ] && die "Usage: $0 <file>"
+
+FILE=$1
+
+test -z ${TELO_SCRIPT_PATH} && TELO_SCRIPT_PATH=/install/vgp-pipeline/telomere/telomere_analysis.sh
+
+test -f $FILE || die "File $FILE not found"
+PREFIX=$(basename $FILE .fasta)
+BED=${PREFIX}/${PREFIX}.windows.0.4.50kb.ends.bed
+
+${TELO_SCRIPT_PATH} ${PREFIX} 0.4 50000 ${FILE}
+test -e ${BED} || die "telomere_analysis.sh failed"
+
+echo -e "FILE\t${FILE}"
+echo -e -n "total telomere regions at the end of contigs:\t"
+cat $BED | wc -l
+echo ""
+echo ""
+cat $BED | cut -f 1  | sort | uniq -c | awk 'BEGIN{t1=0;t2=0;t3=0}{if($1==1){t1+=1}else if($1==2){t2+=1} else {t3+=1}} END{print "telo in one end:\t"t1"\ntelo in two ends:\t"t2"\ntelo more than 2 (must be 0):\t"t3"\n"}'
+
+
+
+
+
